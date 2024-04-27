de5782b7152df5b22145f4b87fb0a7c2ac269d2a
VyshnevskaO <vyshnevskaolenka@gmail.com>
2024-04-27 18:36:55 +0200

    fix issues

---
+++ src/js/header.js
@@ -0,0 +1,18 @@
+
+const menu = document.querySelector(".menu-title");
+const menuList = document.querySelector(".menu-list")
+menu.addEventListener("click", onMenuClick);
+
+function onMenuClick() {
+    if (menuList.classList.contains("visually-hidden-menu")) {
+
+        menuList.classList.remove("visually-hidden-menu");
+        menuList.classList.add("visually-visible-menu");
+
+    } else {
+         menuList.classList.remove("visually-visible-menu");
+        menuList.classList.add("visually-hidden-menu");
+    }
+
+
+}
