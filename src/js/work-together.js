0f64d3996bca5624b8cdfd99c41ceb68aecb61a3
Анна <anna.kuvakina91@gmail.com>
2024-04-27 23:40:27 +0300

    css+js-valid-text

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -2,7 +2,7 @@ const formEl = document.querySelector('.work-form');
 const emailEl = document.querySelector('.work-form input[name=email]');
 const commentsEl = document.querySelector('.work-form input[name=comment]');
 const succesEl = document.querySelector('.work-form-succes');
-const invalidEl = document.querySelector('.work-form-invalid')
+const invalidEl = document.querySelector('.work-form-invalid');
 
 succesEl.style.display = 'none';
 invalidEl.style.display = 'none';
@@ -12,13 +12,20 @@ let formDataEl = {};
 
 fillFormFields();
 
-formEl.addEventListener(
-  'input',
-  event => {
-    formDataEl[event.target.name] = event.target.value;
-    localStorage.setItem(localKey, JSON.stringify(formDataEl));
+formEl.addEventListener('input', event => {
+  formDataEl[event.target.name] = event.target.value;
+  localStorage.setItem(localKey, JSON.stringify(formDataEl));
+
+  if (emailEl.validity.valid) {
+    succesEl.style.display = 'block';
+    invalidEl.style.display = 'none';
+    emailEl.style.borderColor = '#3CBC81';
+  } else {
+    succesEl.style.display = 'none';
+    invalidEl.style.display = 'block';
+    emailEl.style.borderColor = '#E74A3B';
   }
-);
+});
 
 formEl.addEventListener('submit', event => {
   event.preventDefault();
