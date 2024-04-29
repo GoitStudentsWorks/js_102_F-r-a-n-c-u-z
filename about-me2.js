96c6d4870bf2b0bd5e07be353406c8a86bcab006
F-r-a-n-c-u-z <151776886+F-r-a-n-c-u-z@users.noreply.github.com>
2024-04-29 23:02:37 +0300

    about-me.js2.0

---
+++ about-me2.js
@@ -0,0 +1,24 @@
+// // core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import { Navigation } from 'swiper/modules';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+
+// // init Swiper:
+// new Swiper('.swiper-lola', {
+//   // configure Swiper to use modules
+//   modules: [Navigation],
+//   direction: 'horizontal',
+
+//   spaceBetween: 20,
+//   slidesPerView: 5,
+
+//   loop: true,
+//   wrapperClass: 'about-skills-list',
+//   slideClass: 'about-skills-list-div',
+//   slideActiveClass: 'active',
+//   navigation: {
+//     nextEl: '.swiper-button-next'
+//   },
+// });
