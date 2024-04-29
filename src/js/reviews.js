5da5f25c8062c153b8a0c96dcaf9d035a868efd4
EspFerevo <olekchekrenov@gmail.com>
2024-04-30 00:11:11 +0300

    Reviews

---
+++ src/js/reviews.js
@@ -0,0 +1,39 @@
+// 'use strict';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const Swiper = new Swiper('.cards-list', {
+//     keyboard: {
+//       enabled: true,
+//       onlyInViewport: false,
+//     },
+//     direction: 'horizontal',
+//     slidesPerView: 1,
+//     spaceBetween: 16,
+//     navigation: {
+//       nextEl: '.rewiews-button-next',
+//       prevEl: '.rewiews-button-prev',
+//     },
+//     breakpoints: {
+//       768: {
+//         slidesPerView: 2,
+//         spaceBetween: 16,
+//       },
+//       1440: {
+//         slidesPerView: 4,
+//         spaceBetween: 16,
+//       },
+//     },
+//   });
+
+//   document
+//     .querySelector('.rewiews-button-prev')
+//     .addEventListener('click', function () {
+//       mySwiper.slidePrev();
+//     });
+
+//   document
+//     .querySelector('.rewiews-button-next')
+//     .addEventListener('click', function () {
+//       mySwiper.slideNext();
+//     });
+// });
