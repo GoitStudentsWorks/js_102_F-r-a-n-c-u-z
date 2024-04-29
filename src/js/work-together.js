7350054e6fb69f5e9ec57008c4bed3fafd365669
Анна <anna.kuvakina91@gmail.com>
2024-04-29 07:31:30 +0300

    adaptiv+post+btn-sub+modal

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -3,10 +3,17 @@ const emailEl = document.querySelector('.work-form input[name=email]');
 const commentsEl = document.querySelector('.work-form input[name=comment]');
 const succesEl = document.querySelector('.work-form-succes');
 const invalidEl = document.querySelector('.work-form-invalid');
+const ellipsEl = document.querySelector('.work-form-btn-ellipse');
+
+const modalEl = document.querySelector('#work-modal');
+const closeEl = document.querySelector('#work-modal-close');
+const modalTitle = document.querySelector('#work-modal-title');
+const modalMessage = document.querySelector('#work-modal-message');
 
 succesEl.style.display = 'none';
 invalidEl.style.display = 'none';
 
+const BASE_URL = 'https://portfolio-js.b.goit.study/api/requests';
 const localKey = 'work-form-state';
 let formDataEl = {};
 
@@ -27,11 +34,62 @@ formEl.addEventListener('input', event => {
   }
 });
 
+function openModal(title, message) {
+  modalTitle.innerText = title;
+  modalMessage.innerText = message;
+  modalEl.style.display = "block";
+
+  document.addEventListener("DOMContentLoaded", () => {
+    closeEl.onclick = function() {
+        modalEl.style.display = "none";
+    }
+    window.onclick = function(event) {
+      // console.log(event.target);
+      //   if (event.target === modalEl) {
+            modalEl.style.display = "none";
+        // }
+    }
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  closeEl.onclick = function() {
+      modalEl.style.display = "none";
+  }
+  window.onclick = function(event) {
+    console.log(event.target);
+    console.log(modalEl);
+      if (event.target === modalEl) {
+          modalEl.style.display = "none";
+      }
+  }
+})
+
 formEl.addEventListener('submit', event => {
   event.preventDefault();
+  ellipsEl.setAttribute('fill', '#e0373f');
+  succesEl.style.display = 'none';
+  emailEl.style.borderColor = '#1c1d20';
   if (emailEl.value !== '' && commentsEl.value !== '') {
-    console.log(formDataEl);
-    localStorage.removeItem(localKey);
+    
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: JSON.stringify(formDataEl),
+    };
+    fetch(BASE_URL, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(post => openModal(post.title, post.message))
+      .catch(error => console.log(error));
+  
+      localStorage.removeItem(localKey);
     event.currentTarget.reset();
     return;
   }
